519dda4fd4fa55312c8c2ac23e177ebc89982f3e
Joseph Lozano <joseph@digitalonboarding.com>
2020-12-02 10:39:40 -0500

    use digits in file names

--- lib/advent_of_code.ex
+++ lib/advent_of_code.ex
@@ -1,2 +1,16 @@
 defmodule AdventOfCode do
+  alias AdventOfCode.{One, Two}
+
+  def run(day, part) do
+    day_mod =
+      case day do
+        1 -> One
+        2 -> Two
+      end
+
+    case part do
+      1 -> day_mod.part_1
+      2 -> day_mod.part_2
+    end
+  end
 end
