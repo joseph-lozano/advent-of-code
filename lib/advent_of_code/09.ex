76be44f90e580742c432e4ba5bc9fbf1de09f015
Joseph Lozano <joseph@hey.com>
2020-12-09 10:45:27 -0500

    day 9 part 2

--- lib/advent_of_code/09.ex
+++ lib/advent_of_code/09.ex
@@ -19,6 +19,40 @@ defmodule AdventOfCode.Nine do
     end)
   end
 
+  def part_2() do
+    "priv/09.txt"
+    |> File.read!()
+    |> String.trim()
+    |> part_2()
+  end
+
+  def part_2(input, lookback \\ 25) do
+    invalid_number = part_1(input, lookback)
+
+    numbers = parse_input(input)
+    input_size = Enum.count(numbers)
+
+    Enum.reduce_while(0..(input_size - 1), 0, fn a, a_acc ->
+      Enum.reduce_while((a + 1)..(input_size - 1), 0, fn b, b_acc ->
+        slice =
+          numbers
+          |> Enum.slice(a..b)
+
+        sum = Enum.sum(slice)
+
+        cond do
+          sum == invalid_number -> {:halt, Enum.min(slice) + Enum.max(slice)}
+          sum > invalid_number -> {:halt, :gt}
+          true -> {:cont, b_acc}
+        end
+      end)
+      |> case do
+        :gt -> {:cont, a_acc}
+        value -> {:halt, value}
+      end
+    end)
+  end
+
   defp is_sum?(number, numbers) do
     for a <- numbers, b <- numbers do
       a + b == number
--- test/advent_of_code/09_test.exs
+++ test/advent_of_code/09_test.exs
@@ -34,4 +34,37 @@ defmodule AdventOfCode.NineTest do
   test "part 1 real" do
     assert Nine.part_1() == 21_806_024
   end
+
+  test "part 2" do
+    input =
+      """
+      35
+      20
+      15
+      25
+      47
+      40
+      62
+      55
+      65
+      95
+      102
+      117
+      150
+      182
+      127
+      219
+      299
+      277
+      309
+      576
+      """
+      |> String.trim()
+
+    assert Nine.part_2(input, 5) == 62
+  end
+
+  test "part 2 real" do
+    assert Nine.part_2() == 2_986_195
+  end
 end
